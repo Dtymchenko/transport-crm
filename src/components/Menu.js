574f86a08b25d2639d892a096007367f39c24952
Dmytro Tymchenko <timchenkod88@gmail.com>
2023-04-11 22:19:03 +0300

    bg

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -14,8 +14,13 @@ const Menu = () => {
   const dispatch = useDispatch();
   const email = useSelector((state) => state.main?.email);
   const [expanded, setExpanded] = React.useState(false);
+  const Background =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJgAAACACAMAAADAmVDlAAAAWlBMVEUAAAD///+AgIATExNMTEx3d3fn5+dSUlKkpKRra2tvb2/W1taurq4yMjLQ0NBbW1vKysogICC1tbXd3d0ZGRmLi4uSkpLBwcE8PDzv7+8qKiqenp719fUICAjQhERKAAAB7ElEQVR4nO3cyXaCQBBA0SIGUCZHwPH/fzO0KILpTa3qLeptsr1HcqAbrZbkVVkV9cq4uqjKt0fGP81WIG2bOSy15sxLJ1iWW1uW5dkLBnMNshGGuo5jaYDtrBWxdgMMdyFDeSKNtSFeI2trQry1YO6sy7aytybEg7JCD2tAvIfU1oR4tQDv+6FUDtaEeAc5nqwNsU5HIT7Dw1NckswaESsLy57KWvG/alwottaO79r3mr+zlizrPpuR/myN+XTu59u3y7peWYtCq3p9Wewrnzve7se47rPfncFgOUybw7Q5TJvDtDlMm8O0OUybw7Q5TJvDtDlMm8O0TbCsbzf5r2n5pu2zL1iWXq1fW4xd02wOu1l75t0m2L2wtiwr7i8YzDXIRhjsfWKoC7DSWhGrHGAba0SsTSIl8uu3RynAt/yhSpBXcriWArnjfwdloYN+ZlfuPz/2doG9wWIfSdyHOHbZw10ocpfW2M0IePvG3fDycpg2h2lzmDaHaXOYNodpc5g2h2lzmDaHaXOYNodpo/8InPqzeeqgAeyd4jSaQR1mAb7pf47/YAemqCNm2KE87Bgj8kqGwU/sqCzyyzfszHMIOim+5478Yw9JwB4rgT2Ig3t0CfEpnrKPx+EeKAS7mul8+0Y9tCrhHfP1B3s3NIVmpot5AAAAAElFTkSuQmCC";
   React.useEffect(() => {
     dispatch(getUser());
+    const bgChanger = document.querySelector(".navbar-toggler-icon");
+    bgChanger.style.backgroundImage = `url(${Background})`;
+    console.log(bgChanger.style);
   }, [email]);
   const handleMenuClick = () => {
     setExpanded((prev) => !prev);
@@ -45,6 +50,9 @@ const Menu = () => {
     <div ref={burgerRef}>
       <Navbar>
         <Navbar.Toggler
+          // style={{
+          //   backgroundColor: "red",
+          // }}
           target="#navbarToggleExternalContent"
           onClick={() => setExpanded((prev) => !prev)}
         />
